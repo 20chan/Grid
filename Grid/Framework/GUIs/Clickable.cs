becb002ffdffb4537a5b1173fab61b5d2a1c217e
phillyai <sonagi.philly@gmail.com>
2017-05-09 21:52:41 +0900

    Clickable 이벤트 핸들링 구현

--- Grid/Framework/GUIs/Clickable.cs
+++ Grid/Framework/GUIs/Clickable.cs
@@ -25,7 +25,7 @@ namespace Grid.Framework.GUIs
         /// <summary>
         /// 마우스가 UI을 처음 눌렀는가
         /// </summary>
-        public bool IsMouesDown { get; private set; } = false;
+        public bool IsMouseDown { get; private set; } = false;
         /// <summary>
         /// 마우스가 UI에서 뗐는가
         /// </summary>
@@ -45,7 +45,48 @@ namespace Grid.Framework.GUIs
 
         public override void HandleEvent()
         {
+            MouseState mouse = Mouse.GetState();
+            Point cursor = mouse.Position;
 
+            if (!IsInRect(cursor))
+            {
+                if (IsMouseHover) // 막 벗어남
+                {
+                    IsMouseLeaved = true;
+                    IsMouseHover = false;
+                }
+                else if (IsMouseLeaved)
+                    IsMouseLeaved = false;
+            }
+            else
+            {
+                if (!IsMouseHover) // 처음 UI의 범위 안에 들어옴
+                {
+                    IsMouseHover = true;
+                    IsMouseEntered = true;
+                }
+                else if (IsMouseEntered)
+                    IsMouseEntered = false;
+
+                if (mouse.LeftButton == ButtonState.Pressed)
+                {
+                    if (!IsMouseClicking) // 처음 클릭함
+                    {
+                        IsMouseClicking = true;
+                        IsMouseDown = true;
+                    }
+                    else if (IsMouseDown)
+                        IsMouseDown = false;
+                }
+                else if (IsMouseClicking)
+                    IsMouseClicking = false;
+
+                MouseWheel = mouse.ScrollWheelValue;
+            }
+            if(mouse.LeftButton == ButtonState.Released)
+            {
+                if (IsMouseClicking) IsMouseClicking = false;
+            }
         }
 
         public abstract bool IsInRect(Point point);
