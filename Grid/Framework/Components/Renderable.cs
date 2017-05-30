ae754d1e83d9f5994c39697285646487fbeba01c
phillyai <sonagi.philly@gmail.com>
2017-05-30 21:44:58 +0900

    Renderable의 Draw 오버라이드 강제성 취소

--- Grid/Framework/Components/Renderable.cs
+++ Grid/Framework/Components/Renderable.cs
@@ -9,6 +9,6 @@ namespace Grid.Framework.Components
         /// OnCamera가 true이면 Scene에서 MainCamera 위에 투영됩니다. false라면 직접 SpriteBatch.Begin()~End()를 호출해야 합니다.
         /// </summary>
         public bool OnCamera { get; set; } = true;
-        public abstract void Draw(SpriteBatch sb);
+        public virtual void Draw(SpriteBatch sb) { }
     }
 }
