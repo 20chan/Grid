fd9b3d0e3e6d0f8942a90c229efa59d968d9e7e0
phillyai <sonagi.philly@gmail.com>
2017-06-06 22:00:29 +0900

    GUI 인터페이스 추가

---
+++ Grid/Framework/GUIs/IScrollable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grid.Framework.GUIs
+{
+    public interface Scrollable
+    {
+        int MaxScrollValue { get; set; }
+        int MinScrollValue { get; set; }
+        int ScrollValue { get; set; }
+        void Scroll(int value);
+    }
+}
---
+++ Grid/Framework/GUIs/ITypeable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grid.Framework.GUIs
+{
+    public interface ITypeable
+    {
+        bool IsTyping { get; set; }
+        string Text { get; set; }
+    }
+}
