7e77f663156535017bb66b8b88ffaafce0e651f3
phillyai <sonagi.philly@gmail.com>
2017-07-11 13:45:18 +0900

    의미 없는 테스트 코드 제거

--- Grid/Tests/TestScene.cs
+++ Grid/Tests/TestScene.cs
@@ -8,27 +8,14 @@ namespace Grid.Tests
 {
     class TestScene : Scene
     {
-        string buf = "";
         protected override void LoadContent()
         {
             base.LoadContent();
-            var form = new Form1();
-            form.TextEvented += Form_TextEvented;
-            form.Show();
-        }
-
-        private void Form_TextEvented(string obj)
-        {
-            buf = obj;
         }
 
         protected override void Draw(GameTime gameTime)
         {
             base.Draw(gameTime);
-
-            _spriteBatch.Begin();
-            GUI.DrawString(_spriteBatch, Resources.Font, buf, Alignment.Left | Alignment.Top, new Rectangle(0, 0, 500, 300), Color.Black, 0);
-            _spriteBatch.End();
         }
     }
 }
